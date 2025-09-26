2d6d52dd3a37ad25596c456c1578b55c36ff14dd
Yannick Rudolf <yannick.rudolf@yahoo.de>
2025-09-26 12:27:38 +0200

    feat interface-adapters: add createCountryController

---
+++ src/interface-adapters/controllers/create-country.controller.ts
@@ -0,0 +1,26 @@
+import {
+    ICreateCountryUseCase,
+} from '@/src/application/use-cases/create-country.use-case';
+import { Country } from '@/src/entities/models/country';
+
+function presenter(countries: Country[]) {
+    return countries.map((country) => ({
+        id: country.id,
+        name: country.name,
+    }));
+}
+
+export type ICreateCountryController = ReturnType<
+    typeof createCountryController
+>;
+
+export const createCountryController =
+    (createCountryUseCase: ICreateCountryUseCase) => async (input: string) => {
+        // check authentication
+
+        // validate input
+        console.log('Input:', input);
+        const country = await createCountryUseCase(input);
+        return presenter([country]);
+        // call use case
+    };
