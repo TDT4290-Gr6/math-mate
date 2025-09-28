b6b48a17f8ee7bc4513b5267e38d3c5bfd38653e
Yannick Rudolf <yannick.rudolf@yahoo.de>
2025-09-29 01:31:12 +0200

    feat(add-country): add authentication, input checks and comments to add-country example

--- src/application/use-cases/create-country.use-case.ts
+++ src/application/use-cases/create-country.use-case.ts
@@ -1,4 +1,5 @@
 import { ICountriesRepository } from '../repositories/countries.repository.interface';
+import { InputParseError } from '@/src/entities/errors/common';
 import { Country } from '@/src/entities/models/country';
 
 export type ICreateCountryUseCase = ReturnType<typeof createCountryUseCase>;
@@ -6,5 +7,14 @@ export type ICreateCountryUseCase = ReturnType<typeof createCountryUseCase>;
 export const createCountryUseCase =
     (countryRepository: ICountriesRepository) =>
     async (name: string): Promise<Country> => {
+        // HINT: this is where you'd do authorization checks - is this user authorized to create a country
+
+        // business logic input check
+        if (name.length < 1) {
+            throw new InputParseError(
+                'Country name must be at least 1 character long.',
+            );
+        }
+
         return countryRepository.createCountry({ name });
     };
--- src/interface-adapters/controllers/create-country.controller.ts
+++ src/interface-adapters/controllers/create-country.controller.ts
@@ -1,24 +1,43 @@
 import { ICreateCountryUseCase } from '@/src/application/use-cases/create-country.use-case';
-import { Country } from '@/src/entities/models/country';
+import { IAuthenticationService } from '@/src/application/services/auth.service.interface';
+import { countryPresenter } from '../presenters/country.presenter';
+import { UnauthenticatedError } from '@/src/entities/errors/auth';
+import { InputParseError } from '@/src/entities/errors/common';
+import { z } from 'zod';
 
-function presenter(countries: Country[]) {
-    return countries.map((country) => ({
-        id: country.id,
-        name: country.name,
-    }));
-}
+const inputSchema = z.object({ name: z.string().min(1) });
 
 export type ICreateCountryController = ReturnType<
     typeof createCountryController
 >;
 
 export const createCountryController =
-    (createCountryUseCase: ICreateCountryUseCase) => async (input: string) => {
+    (
+        createCountryUseCase: ICreateCountryUseCase,
+        authenticationService: IAuthenticationService,
+    ) =>
+    async (input: string, sessionId: string) => {
         // check authentication
+        const isAuthenticated =
+            await authenticationService.validateSession(sessionId);
+        if (!isAuthenticated) {
+            throw new UnauthenticatedError('User must be logged in.');
+        }
 
         // validate input
-        console.log('Input:', input);
-        const country = await createCountryUseCase(input);
-        return presenter([country]);
+        const { data, error: inputParseError } = inputSchema.safeParse(input);
+
+        if (inputParseError) {
+            throw new InputParseError('Invalid data', {
+                cause: inputParseError,
+            });
+        }
+
+        const { name } = data;
+
         // call use case
+        const country = await createCountryUseCase(name);
+
+        // format output
+        return countryPresenter([country]);
     };
