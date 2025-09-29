fbf9dff1c2c97dabe8e9681a9973118b758ed986
Yannick Rudolf <yannick.rudolf@yahoo.de>
2025-09-29 02:10:37 +0200

    fix(use-case): remove unnecessary input validation

--- src/application/use-cases/create-country.use-case.ts
+++ src/application/use-cases/create-country.use-case.ts
@@ -1,5 +1,4 @@
 import { ICountriesRepository } from '../repositories/countries.repository.interface';
-import { InputParseError } from '@/src/entities/errors/common';
 import { Country } from '@/src/entities/models/country';
 
 export type ICreateCountryUseCase = ReturnType<typeof createCountryUseCase>;
@@ -8,13 +7,6 @@ export const createCountryUseCase =
     (countryRepository: ICountriesRepository) =>
     async (name: string): Promise<Country> => {
         // HINT: this is where you'd do authorization checks - is this user authorized to create a country
-
-        // business logic input check
-        if (name.length < 1) {
-            throw new InputParseError(
-                'Country name must be at least 1 character long.',
-            );
-        }
 
         return countryRepository.createCountry({ name });
     };
