ca9dc98e5220151e7e4dc38d334dd111e3fe1bd0
mariusOstro <marius.w.ostro@gmail.com>
2025-10-01 01:06:25 +0200

    added comment

---
+++ supabase-client.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+//Supabase URL and Key from .env file is posted to teams/extramaterial/dotenv
+
+const supabaseUrl = process.env.DATABASE_URL as string;
+const supabaseKey = process.env.SUPABASE_KEY as string;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
