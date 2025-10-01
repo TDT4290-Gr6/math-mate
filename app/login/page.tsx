78a69c1a4d2490cf394ca24448984e799fc366c0
Axelhovde <160112522+Axelhovde@users.noreply.github.com>
2025-10-01 13:19:45 +0000

    fix(format): autofix code style

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,18 @@
-"use client";
+'use client';
 
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function LoginPage() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-6 bg-[var(--color-background)]">
-      <h1 className="text-2xl font-bold">Login Page</h1>
-      <Link
-        //TODO: Change when auth is implemented
-        href="/protected/dashboard"
-        className="px-4 py-2 bg-[var(--color-accent)] text-white rounded-lg shadow"
-      >
-        Go to Dashboard
-      </Link>
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen gap-6 bg-[var(--color-background)]">
+            <h1 className="text-2xl font-bold">Login Page</h1>
+            <Link
+                //TODO: Change when auth is implemented
+                href="/protected/dashboard"
+                className="px-4 py-2 bg-[var(--color-accent)] text-white rounded-lg shadow"
+            >
+                Go to Dashboard
+            </Link>
+        </div>
+    );
 }
