bf57546a6ebd720e52dd0ee921cc54af128b0e41
juliadai8 <juliadai03@gmail.com>
2025-10-01 22:19:34 +0200

    feat(ci): add PR trigger and npm cache to lighthouse workflow

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
     ci: {
         collect: {
-            url: ['http://localhost:3000'],
+            url: ['http://localhost:3000' /* Add other pages to test */],
             startServerCommand: 'npm start',
             startServerReadyPattern: 'ready on',
             startServerReadyTimeout: 30000,
