5cf1b7172a23971273351c4a0e7b24d33d3d9f12
juliadai8 <juliadai03@gmail.com>
2025-10-01 21:32:02 +0200

    fix(ci): update lighthouse config

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,20 +1,17 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: './.next',
-      url: [
-        'http://localhost:3000',
-      ],
+      url: ['http://localhost:3000'],
       startServerCommand: 'npm start',
       startServerReadyPattern: 'ready on',
       startServerReadyTimeout: 30000,
     },
     assert: {
       assertions: {
-        'categories:performance': ['warn', { minScore: 0.9 }],
+        'categories:performance': ['warn', { minScore: 0.8 }],
         'categories:accessibility': ['error', { minScore: 0.9 }],
-        'categories:best-practices': ['warn', { minScore: 0.9 }],
-        'categories:seo': ['warn', { minScore: 0.9 }],
+        'categories:best-practices': ['warn', { minScore: 0.8 }],
+        'categories:seo': ['warn', { minScore: 0.8 }],
       },
     },
     upload: {
