cfa1555d627a9e957583b3fdafc4ca30a934f9af
runarmod <40912933+runarmod@users.noreply.github.com>
2025-10-01 15:19:13 +0000

    fix(format): autofix code style

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,18 @@
-"use client";
+'use client';
 
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function LoginPage() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-6">
-      <h1 className="text-2xl font-bold">Login Page</h1>
-      <Link
-        //TODO: Change when auth is implemented
-        href="/protected/dashboard"
-        className="px-4 py-2 bg-indigo-600 text-white rounded-lg shadow hover:bg-indigo-700"
-      >
-        Go to Dashboard
-      </Link>
-    </div>
-  );
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center gap-6">
+            <h1 className="text-2xl font-bold">Login Page</h1>
+            <Link
+                //TODO: Change when auth is implemented
+                href="/protected/dashboard"
+                className="rounded-lg bg-indigo-600 px-4 py-2 text-white shadow hover:bg-indigo-700"
+            >
+                Go to Dashboard
+            </Link>
+        </div>
+    );
 }
--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,20 +1,21 @@
 // app/layout.tsx (server component)
-import { redirect } from "next/navigation";
+import { redirect } from 'next/navigation';
 
-export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
+export default function ProtectedLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    // block all pages except login if not authenticated
+    //TODO: Replace with real auth check
+    if (false) {
+        redirect('/login');
+    }
 
-  // block all pages except login if not authenticated
-  //TODO: Replace with real auth check
-  if (false) {
-    redirect("/login");
-  }
-
-  return (
-    
-    <div className="min-h-screen flex flex-col bg-gray-50">
-      {/* optional shared navbar or sidebar for protected pages */}
-      {children}
-    </div>
-    
-  );
+    return (
+        <div className="flex min-h-screen flex-col bg-gray-50">
+            {/* optional shared navbar or sidebar for protected pages */}
+            {children}
+        </div>
+    );
 }
