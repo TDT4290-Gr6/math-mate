88740f870bafe2828defae42ec86ceff8f31c9b6
Axel <axel.s.hovde@gmail.com>
2025-10-01 10:02:26 +0200

    feat(routing-setup): Set up middleware

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ export default function LoginPage() {
     <div className="flex flex-col items-center justify-center min-h-screen gap-6">
       <h1 className="text-2xl font-bold">Login Page</h1>
       <Link
-        href="/protected/dashboard"
+        href="/dashboard"
         className="px-4 py-2 bg-indigo-600 text-white rounded-lg shadow hover:bg-indigo-700"
       >
         Go to Dashboard
