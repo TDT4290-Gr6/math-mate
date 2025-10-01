5630d42b339b2a12d6dc37a2f8805732802310cd
mariusOstro <147614453+mariusOstro@users.noreply.github.com>
2025-10-01 10:57:43 +0000

    fix(format): autofix code style

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 // app/page.tsx
-import { redirect } from "next/navigation";
+import { redirect } from 'next/navigation';
 /* import { getCurrentUser } from "@/src/application/getCurrentUser"; */
 
 export default async function HomePage() {
-  /* const user = await getCurrentUser(); // server-side check */
+    /* const user = await getCurrentUser(); // server-side check */
 
-  // TODO: Replace with real auth check
-  const isAuthenticated = false; // Replace with actual auth logic
-  if (isAuthenticated) {
-    redirect("/protected/dashboard"); // logged-in users go to dashboard
-  } else {
-    redirect("/login"); // not logged-in users go to login
-  }
+    // TODO: Replace with real auth check
+    const isAuthenticated = false; // Replace with actual auth logic
+    if (isAuthenticated) {
+        redirect('/protected/dashboard'); // logged-in users go to dashboard
+    } else {
+        redirect('/login'); // not logged-in users go to login
+    }
 
-  return null; // nothing renders because of redirect
+    return null; // nothing renders because of redirect
 }
