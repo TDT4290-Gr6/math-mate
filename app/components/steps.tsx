67b8b1c527422be92675ea7efe3cc00fe48a5bcf
juliadai8 <125892682+juliadai8@users.noreply.github.com>
2025-10-02 10:53:19 +0000

    fix(format): autofix code style

--- app/components/steps.tsx
+++ app/components/steps.tsx
@@ -1,24 +1,28 @@
-import { Minus } from "lucide-react";
+import { Minus } from 'lucide-react';
 
 interface Step {
-    stepID: string,
-    content: string 
+    stepID: string;
+    content: string;
 }
 
 interface StepsProps {
-    steps: Array<Step>,
-    currentStep: number
+    steps: Array<Step>;
+    currentStep: number;
 }
 
-export default function Steps({steps, currentStep}: StepsProps ) {
-    const visibleSteps: Step[] = steps.slice(0, currentStep)
+export default function Steps({ steps, currentStep }: StepsProps) {
+    const visibleSteps: Step[] = steps.slice(0, currentStep);
 
     return (
         <div className="space-y-2">
             {visibleSteps.map((step, index) => (
                 <div key={step.stepID} className="p-4">
-                    <h3 className="font-semibold text-lg mb-2 flex flex-row">
-                        <Minus color="#EB5E28" strokeWidth={6} className="mr-2 self-center"/>
+                    <h3 className="mb-2 flex flex-row text-lg font-semibold">
+                        <Minus
+                            color="#EB5E28"
+                            strokeWidth={6}
+                            className="mr-2 self-center"
+                        />
                         Step {index + 1}
                     </h3>
                     <p>{step.content}</p>
