392b57115c852c9241230a8f9c1be80f9c2e30b5
Yannick Rudolf <yannick.rudolf@yahoo.de>
2025-10-02 13:07:56 +0200

    fix(middleware): Add matcher configuration to avoid protecting static assets

--- middleware.ts
+++ middleware.ts
@@ -5,3 +5,17 @@ export default withAuth({
         signIn: '/login', // Redirect unauthenticated users to /login
     },
 });
+
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api/auth (NextAuth routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * - public folder
+         */
+        '/((?!api/auth|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    ],
+};
