9898149234d9d6e89310abe5ec7c4298cbfbc0d4
Yannick Rudolf <yannick.rudolf@yahoo.de>
2025-10-02 20:52:56 +0200

    docs(createUserUseCase): add comment

--- src/application/use-cases/create-user.use-case.ts
+++ src/application/use-cases/create-user.use-case.ts
@@ -6,5 +6,6 @@ export type ICreateUserUseCase = ReturnType<typeof createUserUseCase>;
 export const createUserUseCase =
     (userRepository: IUsersRepository) =>
     async (uuid: string): Promise<User> => {
+        // apparently not really a uuid but the id passed from next-auth
         return await userRepository.createUser({ uuid });
     };
