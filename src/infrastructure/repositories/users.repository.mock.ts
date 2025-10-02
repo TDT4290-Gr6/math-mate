f1770c3adda3c0fa86e716f0df99582c0f306c92
Yannick Rudolf <yannick.rudolf@yahoo.de>
2025-10-02 20:45:12 +0200

    fix(mock user repo): typo

--- src/infrastructure/repositories/users.repository.mock.ts
+++ src/infrastructure/repositories/users.repository.mock.ts
@@ -8,12 +8,23 @@ export class MockUsersRepository implements IUsersRepository {
         this._users = [];
     }
     async createUser(user: UserInsert): Promise<User> {
+        // check if user with same uuid exists
+        const existingUser = this._users.find((u) => u.uuid === user.uuid);
+        if (existingUser) {
+            console.log(
+                'MockUsersRepository: User already exists',
+                existingUser,
+            );
+            return existingUser;
+        }
+
         const newUser: User = {
             id: this._users.length + 1,
             ...user,
             score: 0,
         };
         this._users.push(newUser);
+        console.log('MockUsersRepository: Created user', newUser);
         return newUser;
     }
 }
