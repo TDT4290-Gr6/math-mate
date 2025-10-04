e5032e621dd02262ebf002512e9327eb6df99357
Axelhovde <160112522+Axelhovde@users.noreply.github.com>
2025-10-04 15:05:27 +0000

    fix(format): autofix code style

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,38 +1,39 @@
 'use client';
-import { Button } from "./components/ui/button";
-import Title from "./components/ui/title";
-import { PanelsTopLeft, Search } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { PanelsTopLeft, Search } from 'lucide-react';
+import { Button } from './components/ui/button';
+import { useRouter } from 'next/navigation';
+import Title from './components/ui/title';
 
 export default function NotFoundPage() {
     const router = useRouter();
 
     return (
         <div className="flex min-h-screen flex-col items-center justify-center gap-6">
-            <div className="max-w-1/2 max-h-5xl bg-[var(--card)] rounded-[30px] shadow-sm flex flex-col items-center justify-center">
-                
+            <div className="max-h-5xl flex max-w-1/2 flex-col items-center justify-center rounded-[30px] bg-[var(--card)] shadow-sm">
                 {/* row for title and icons */}
                 <div className="flex flex-row items-center">
                     {/* Title section */}
-                    <div className="flex flex-row w-full p-12">
-                        <div className="flex flex-col"> 
+                    <div className="flex w-full flex-row p-12">
+                        <div className="flex flex-col">
                             <Title title="404" size={120} />
                             <div className="flex flex-row py-4">
-                                <div className="w-[32px] h-[16px] bg-[var(--accent)] rounded-[5px] mr-2 mt-2"></div>
-                                <h1 className="text-[var(--fg)] text-xl text-center md:text-2xl">Page Not Found</h1>
+                                <div className="mt-2 mr-2 h-[16px] w-[32px] rounded-[5px] bg-[var(--accent)]"></div>
+                                <h1 className="text-center text-xl text-[var(--fg)] md:text-2xl">
+                                    Page Not Found
+                                </h1>
                             </div>
                         </div>
                     </div>
                     {/* stack with search icon on top of panels icon */}
-                    <div className="relative px-12 lg:block hidden">
-                        <PanelsTopLeft className="h-36 w-36 text-[var(--primary)] opacity-20 mr-8" />
-                        
+                    <div className="relative hidden px-12 lg:block">
+                        <PanelsTopLeft className="mr-8 h-36 w-36 text-[var(--primary)] opacity-20" />
+
                         {/* Search icon */}
-                        <Search className="absolute top-[-40px] left-26 h-36 w-36 text-[var(--primary)] mr-8" />
+                        <Search className="absolute top-[-40px] left-26 mr-8 h-36 w-36 text-[var(--primary)]" />
 
                         {/* Lupe handle */}
                         <div
-                            className="absolute bg-[var(--accent)] rounded-[5px] w-[50px] h-[16px]"
+                            className="absolute h-[16px] w-[50px] rounded-[5px] bg-[var(--accent)]"
                             style={{
                                 transform: 'rotate(45deg)',
                                 top: '80px',
@@ -43,16 +44,20 @@ export default function NotFoundPage() {
                 </div>
                 {/* Description */}
                 <div className="">
-                    <p className="text-[var(--fg)] text-md text-center md:text-lg md:px-14 px-4">The page you are looking for might have been removed, had its name changed or is temporary unavailable.</p>
+                    <p className="text-md px-4 text-center text-[var(--fg)] md:px-14 md:text-lg">
+                        The page you are looking for might have been removed,
+                        had its name changed or is temporary unavailable.
+                    </p>
                 </div>
                 {/* Go back to menu button */}
-                <Button 
-                    variant="default" 
-                    className="bg-[var(--accent)] w-54 m-8"
-                    onClick={() => router.push('/protected/start')
-                }>Go back to menu</Button>
+                <Button
+                    variant="default"
+                    className="m-8 w-54 bg-[var(--accent)]"
+                    onClick={() => router.push('/protected/start')}
+                >
+                    Go back to menu
+                </Button>
             </div>
-            
         </div>
     );
 }
