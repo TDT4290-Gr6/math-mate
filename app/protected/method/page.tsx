d7a478b2260de898f117318a81bc1643f78d776c
Axel <axel.s.hovde@gmail.com>
2025-10-04 13:50:33 +0200

    feat/#30(sidebar-menu): Merged in changes from main

--- app/protected/method/page.tsx
+++ app/protected/method/page.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import Link from 'next/link';
+import Header from '@/components/ui/header';
 
 /**
  * The page component that displays a set of method cards to help solve
@@ -16,23 +17,9 @@ export default function MethodPage() {
     const router = useRouter();
     return (
         <div className="flex min-h-screen flex-col items-center gap-6">
-            <div className="flex w-full flex-col border-b-1">
-                <div className="flex w-full flex-row justify-between px-10 py-6">
-                    {/* TODO: Replace with backbutton */}
-                    <div className="h-10 bg-[var(--accent)]">
-                        <Link
-                            href="/protected/dashboard"
-                            className="rounded-lg bg-[var(--accent)] px-4 py-2 text-white shadow hover:bg-[var(--accent-hover)]"
-                        >
-                            Go to Dashboard
-                        </Link>
-                    </div>
-                    {/* TODO: Replace with hamburger :) */}
-                    <div className="h-10 w-10 bg-[var(--accent)]"></div>
-                </div>
+
                 {/* TODO: Replace with question component :) */}
-                <div className="flex h-40 w-5xl flex-row items-center gap-4"></div>
-            </div>
+                <Header variant="question" mathQuestion={<div className="flex h-40 w-5xl flex-row items-center justify-center gap-4"> <p>TODO fix question component</p></div>} />
 
             <div className="px-[15%] pt-4">
                 <p className="text-[var(--foreground)]">
