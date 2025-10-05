f53ce6e0b6bc8d5f88ca08fc97f7c2e3a6dc49f1
Runar Saur Modahl <runarmod@gmail.com>
2025-10-05 22:31:55 +0200

    fix(startpage): add header to startpage

--- app/protected/start/page.tsx
+++ app/protected/start/page.tsx
@@ -10,6 +10,7 @@ import {
 } from '@/components/ui/card';
 import { Subject, subjectIcons } from '../../constants/subjects';
 import SubjectCheckbox from '@/components/subject-checkbox';
+import Header from '@/components/ui/header';
 import { ChevronRight } from 'lucide-react';
 import { useLocalStorage } from 'react-use';
 import { useState, useEffect } from 'react';
@@ -39,59 +40,64 @@ export default function StartPage() {
     }
 
     return (
-        <div className="flex min-h-screen items-center justify-center">
-            <Card className="relative w-2xl p-5">
-                <CardHeader>
-                    <CardTitle>
-                        <Image
-                            src="/wide-logo-light.svg" // TODO: dynamically switch between light and dark mode logos
-                            alt="MathMate"
-                            width={(313 * 2) / 3}
-                            height={(110 * 2) / 3}
-                            className="m-2"
-                        />
-                    </CardTitle>
-                    <CardDescription className="relative">
-                        This website is made to help students learn math in a
-                        simple and supportive way. You can explore step-by-step
-                        solutions to math problems, so you not only see the
-                        answer but also understand how to get there.
-                        <div className="bg-primary absolute top-0 -left-15 h-full w-8 rounded-sm"></div>
-                    </CardDescription>
-                </CardHeader>
-                <CardContent className="flex flex-col items-center gap-4">
-                    <p>
-                        Choose which categories of math you want to work with:
-                    </p>
-                    <div className="flex flex-wrap justify-center gap-2">
-                        {Object.keys(subjectIcons).map((subject) => (
-                            <SubjectCheckbox
-                                key={subject}
-                                subject={subject as Subject}
-                                // Set checked to false on initial render, then update based on local storage
-                                checked={
-                                    isHydrated &&
-                                    (selectedSubjects?.includes(
-                                        subject as Subject,
-                                    ) ??
-                                        false)
-                                }
-                                onToggle={toggleSubject}
+        <>
+            <Header variant="simple" className="fixed" />
+            <div className="flex min-h-screen flex-col items-center justify-center">
+                <Card className="relative w-2xl p-5">
+                    <CardHeader>
+                        <CardTitle>
+                            <Image
+                                src="/wide-logo-light.svg" // TODO: dynamically switch between light and dark mode logos
+                                alt="MathMate"
+                                width={(313 * 2) / 3}
+                                height={(110 * 2) / 3}
+                                className="m-2"
                             />
-                        ))}
-                    </div>
-                    Ready? Then press {'"Start Practicing"'} and get your first
-                    math question.
-                </CardContent>
-                <CardFooter>
-                    <Link
-                        href="/protected/dashboard" // TODO: Link to correct page when it is created
-                        className="bg-primary hover:bg-primary/90 text-background absolute right-24 -bottom-5 flex gap-2 rounded-full px-6 py-2"
-                    >
-                        Start Practicing <ChevronRight className="-mr-2" />
-                    </Link>
-                </CardFooter>
-            </Card>
-        </div>
+                        </CardTitle>
+                        <CardDescription className="relative">
+                            This website is made to help students learn math in
+                            a simple and supportive way. You can explore
+                            step-by-step solutions to math problems, so you not
+                            only see the answer but also understand how to get
+                            there.
+                            <div className="bg-primary absolute top-0 -left-15 h-full w-8 rounded-sm"></div>
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent className="flex flex-col items-center gap-4">
+                        <p>
+                            Choose which categories of math you want to work
+                            with:
+                        </p>
+                        <div className="flex flex-wrap justify-center gap-2">
+                            {Object.keys(subjectIcons).map((subject) => (
+                                <SubjectCheckbox
+                                    key={subject}
+                                    subject={subject as Subject}
+                                    // Set checked to false on initial render, then update based on local storage
+                                    checked={
+                                        isHydrated &&
+                                        (selectedSubjects?.includes(
+                                            subject as Subject,
+                                        ) ??
+                                            false)
+                                    }
+                                    onToggle={toggleSubject}
+                                />
+                            ))}
+                        </div>
+                        Ready? Then press {'"Start Practicing"'} and get your
+                        first math question.
+                    </CardContent>
+                    <CardFooter>
+                        <Link
+                            href="/protected/dashboard" // TODO: Link to correct page when it is created
+                            className="bg-primary hover:bg-primary/90 text-background absolute right-24 -bottom-5 flex gap-2 rounded-full px-6 py-2"
+                        >
+                            Start Practicing <ChevronRight className="-mr-2" />
+                        </Link>
+                    </CardFooter>
+                </Card>
+            </div>
+        </>
     );
 }
