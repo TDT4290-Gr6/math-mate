aab9f1c86d731b101772d18200ae45784c0326c5
Runar Saur Modahl <runarmod@gmail.com>
2025-10-06 14:09:30 +0200

    chore(subject-select): use kebab-case for file name (#41)

--- app/protected/start/page.tsx
+++ app/protected/start/page.tsx
@@ -8,7 +8,7 @@ import {
     CardHeader,
     CardTitle,
 } from '@/components/ui/card';
-import SubjectSelect from '@/components/ui/subjectSelect';
+import SubjectSelect from '@/components/ui/subject-select';
 import { ChevronRight } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
