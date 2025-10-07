de786f606ac8baaa057bbe9361ce1008b558555a
Jennytl <158496566+Jennytl@users.noreply.github.com>
2025-10-07 07:59:14 +0000

    fix(format): autofix code style

--- app/components/ui/question-card.tsx
+++ app/components/ui/question-card.tsx
@@ -1,16 +1,13 @@
-import Title from "./title";
-
+import Title from './title';
 
 interface QuestionCardProps {
-  description: string;
+    description: string;
 }
 
-export default function QuestionCard({
-    description,
-}: QuestionCardProps) {
+export default function QuestionCard({ description }: QuestionCardProps) {
     return (
         <div className="mx-4 my-4 max-w-[50vw] rounded-xl bg-[var(--background)] p-4 shadow-sm">
-            <Title title={"Question: "} />
+            <Title title={'Question: '} />
 
             <p className="mb-4 line-clamp-7 text-[var(--foreground)]">
                 {description}
@@ -18,7 +15,3 @@ export default function QuestionCard({
         </div>
     );
 }
-
-
-
-
--- app/protected/question/page.tsx
+++ app/protected/question/page.tsx
@@ -1,19 +1,18 @@
 'use client';
 
-import { Button } from "@/components/ui/button";
-import Title from "@/components/ui/title";
-import { useState } from "react";
-
+import { Button } from '@/components/ui/button';
+import Title from '@/components/ui/title';
+import { useState } from 'react';
 
 export default function QuestionPage() {
-
-  // TODO: api call for fetcing question description
-  const [description, setDescription] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in nunc diam. Fusce accumsan tempor justo ac pellentesque. Etiam varius at quam sit amet varius. Sed ligula nunc, tristique eu lacus at, suscipit laoreet arcu. Vestibulum dictum egestas sem at sodales. Suspendisse nisi enim, accumsan nec aliquet sed, pretium ut justo. Aliquam egestas nunc eget lacus sollicitudin, vel porta augue faucibus. Sed vehicula maximus aliquam. Suspendisse volutpat in mi viverra malesuada.");
-
+    // TODO: api call for fetcing question description
+    const [description, setDescription] = useState(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in nunc diam. Fusce accumsan tempor justo ac pellentesque. Etiam varius at quam sit amet varius. Sed ligula nunc, tristique eu lacus at, suscipit laoreet arcu. Vestibulum dictum egestas sem at sodales. Suspendisse nisi enim, accumsan nec aliquet sed, pretium ut justo. Aliquam egestas nunc eget lacus sollicitudin, vel porta augue faucibus. Sed vehicula maximus aliquam. Suspendisse volutpat in mi viverra malesuada.',
+    );
 
     return (
         <div className="mx-4 my-4 max-w-[50vw] rounded-xl bg-[var(--background)] p-4 shadow-sm">
-            <Title title={"Question: "} />
+            <Title title={'Question: '} />
 
             <p className="mb-4 line-clamp-7 text-[var(--foreground)]">
                 {description}
