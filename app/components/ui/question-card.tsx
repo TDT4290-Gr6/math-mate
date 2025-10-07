2f112671f7c5d0083bfe90544c9c1dbdae13ba63
Jenny Lindgaard <jenny.lindgaard@icloud.com>
2025-10-07 21:01:25 +0200

    fix(question-card): style of card

--- app/components/ui/question-card.tsx
+++ app/components/ui/question-card.tsx
@@ -6,9 +6,8 @@ interface QuestionCardProps {
 
 export default function QuestionCard({ description }: QuestionCardProps) {
     return (
-        <div className="mx-4 my-4 max-w-[50vw] rounded-xl bg-[var(--background)] p-4 shadow-sm">
+        <div className="mx-4 my-4 max-w-[50vw] rounded-xl bg-[var(--card)] p-4 shadow-sm">
             <Title title={'Question: '} />
-
             <p className="mb-4 line-clamp-7 text-[var(--foreground)]">
                 {description}
             </p>
