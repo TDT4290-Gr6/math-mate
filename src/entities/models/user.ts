31926bd47aae67f499a1701eacc5fca04b808921
Yannick Rudolf <yannick.rudolf@yahoo.de>
2025-10-09 09:24:42 +0200

    fix(user): update type of uuid. cannot be guaranteed be a uuid therefore changed to string

--- src/entities/models/user.ts
+++ src/entities/models/user.ts
@@ -2,7 +2,7 @@ import { countrySchema } from './country';
 import { z } from 'zod';
 
 export const userSchema = z.object({
-    uuid: z.uuid(),
+    uuid: z.string(),
     id: z.int().min(1),
     score: z.float64().min(0).max(1).default(0),
     country: countrySchema.shape.id,
