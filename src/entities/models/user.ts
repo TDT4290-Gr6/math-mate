fc7c82dafaddc413980b3ef339e9ff1a71aa5b91
Yannick Rudolf <yannick.rudolf@yahoo.de>
2025-10-09 13:48:34 +0200

    fix(user): make consistent with database

--- src/entities/models/user.ts
+++ src/entities/models/user.ts
@@ -4,15 +4,14 @@ import { z } from 'zod';
 export const userSchema = z.object({
     uuid: z.string(),
     id: z.int().min(1),
-    score: z.float64().min(0).max(1).default(0),
-    countryId: countrySchema.shape.id,
+    score: z.float64().min(0).max(1).default(0).optional(),
+    countryId: countrySchema.shape.id.optional(),
 });
 
 export type User = z.infer<typeof userSchema>;
 
 export const userInsertSchema = userSchema.pick({
     uuid: true,
-    countryId: true,
 });
 
 export type UserInsert = z.infer<typeof userInsertSchema>;
