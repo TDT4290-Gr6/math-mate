8417b06aa107f5644149cb35e3214af4ab62b25f
Yannick Rudolf <yannick.rudolf@yahoo.de>
2025-10-09 14:58:35 +0200

    fix(tests): rm tests from repo again

--- vitest.config.ts
+++
@@ -1,8 +0,0 @@
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { defineConfig } from 'vitest/config';
-
-// npm install vitest
-// npm install vite-tsconfig-paths
-export default defineConfig({
-    plugins: [tsconfigPaths()],
-});
