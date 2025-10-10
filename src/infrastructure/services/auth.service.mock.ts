a5460e99d0c217d3df00611f9f836558947f0c3c
Yannick Rudolf <yannick.rudolf@yahoo.de>
2025-10-10 16:41:32 +0200

    fix(mock auth service): add getCurrentUserId

--- src/infrastructure/services/auth.service.mock.ts
+++ src/infrastructure/services/auth.service.mock.ts
@@ -1,5 +1,8 @@
 import type { IAuthenticationService } from '@/application/services/auth.service.interface';
 export class MockAuthenticationService implements IAuthenticationService {
+    async getCurrentUserId(): Promise<string | null> {
+        return 'mock-user-id';
+    }
     async isAuthenticated(): Promise<boolean> {
         // Simulate session validation
         return true;
