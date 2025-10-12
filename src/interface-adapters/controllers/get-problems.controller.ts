a6a4c30118a602e5aeaea842d5251b16536bd5c0
Jenny Lindgaard <jenny.lindgaard@icloud.com>
2025-10-12 21:30:23 +0200

    fix(get-problems.controller): add upper limit for input schema (#44)

--- src/interface-adapters/controllers/get-problems.controller.ts
+++ src/interface-adapters/controllers/get-problems.controller.ts
@@ -7,7 +7,7 @@ import { z } from 'zod';
 
 const inputSchema = z.object({
     offset: z.number().nonnegative(),
-    limit: z.number().positive(),
+    limit: z.number().positive().max(100),
     subjects: z.array(z.string()).optional(),
 });
 
