ef8a55bd1d898b55193e18e4c80a6e70b8cf7291
Jenny Lindgaard <jenny.lindgaard@icloud.com>
2025-10-12 12:39:22 +0200

    feat(actions): use defined types (#44)

--- app/actions.ts
+++ app/actions.ts
@@ -1,12 +1,13 @@
 'use server';
 
 import { getInjection } from '@/di/container';
+import { Problem } from './types';
 
 export async function getProblems(
     offset: number,
     limit: number,
     subjects: string[],
-) {
+): Promise<Problem[]> {
     const getProblemsController = getInjection('IGetProblemsController');
     const problems = getProblemsController({ offset, limit, subjects });
     return problems;
