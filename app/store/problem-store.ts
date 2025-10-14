cfe5f3a6df8e9be2c7297f44b6889f15c30d1603
Jenny Lindgaard <jenny.lindgaard@icloud.com>
2025-10-14 09:19:48 +0200

    feat(problem-store): add persistence (#86)

--- app/store/problem-store.ts
+++ app/store/problem-store.ts
@@ -1,10 +1,19 @@
+import { persist, createJSONStorage } from 'zustand/middleware';
 import type { Problem } from '@/entities/models/problem';
 import { create } from 'zustand';
 
 export const useProblemStore = create<{
     problem: Problem | null;
     setProblem: (p: Problem) => void;
-}>((set) => ({
-    problem: null,
-    setProblem: (p) => set({ problem: p }),
-}));
+}>()(
+    persist(
+        (set) => ({
+            problem: null,
+            setProblem: (p) => set({ problem: p }),
+        }),
+        {
+            name: 'problem-storage', // unique name for localStorage key
+            storage: createJSONStorage(() => localStorage),
+        },
+    ),
+);
