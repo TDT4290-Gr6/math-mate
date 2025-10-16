262f094469d92a696c241bf4970545b1569ab803
Jenny Lindgaard <158496566+Jennytl@users.noreply.github.com>
2025-10-16 08:58:14 +0200

    Update src/interface-adapters/presenters/problems.presenter.ts
    Co-authored-by: Runar Saur Modahl <40912933+runarmod@users.noreply.github.com>

--- src/interface-adapters/presenters/problems.presenter.ts
+++ src/interface-adapters/presenters/problems.presenter.ts
@@ -1,13 +1,6 @@
-import { Problem } from '@/entities/models/problem';
+import type { Problem } from '@/entities/models/problem';
+import { problemPresenter } from './problem.presenter';
 
 export function problemsPresenter(problems: Problem[]) {
-    return problems.map((problem) => ({
-        id: problem.id,
-        problem: problem.problem,
-        solution: problem.solution,
-        subject: problem.subject,
-        level: problem.level,
-        methods: problem.methods,
-        title: problem.title,
-    }));
+    return problems.map(problemPresenter);
 }
