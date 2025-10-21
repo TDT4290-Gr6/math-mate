4c4cade4bbb83ee3ab07c9b7f918d7cfc54feb44
juliadai8 <125892682+juliadai8@users.noreply.github.com>
2025-10-21 19:03:30 +0000

    fix(format): autofix code style

--- app/components/sidebar-menu.tsx
+++ app/components/sidebar-menu.tsx
@@ -188,7 +188,7 @@ export default function SidebarMenu({ onClose }: SidebarMenuProps) {
                     {/* Logout button */}
                     <button
                         type="button"
-                        className="bg-sidebar-primary hover:bg-accent flex cursor-pointer items-center gap-2 rounded-4xl px-4 py-2"  
+                        className="bg-sidebar-primary hover:bg-accent flex cursor-pointer items-center gap-2 rounded-4xl px-4 py-2"
                         onClick={handleLogOut}
                     >
                         <UserRound
