c392c26f16a93b7ae5158b3dec7eaf98544d2fa8
Runar Saur Modahl <runarmod@gmail.com>
2025-10-22 21:17:03 +0200

    fix(eventcontroller): do not throw when no userId

--- src/interface-adapters/controllers/create-event.controller.ts
+++ src/interface-adapters/controllers/create-event.controller.ts
@@ -1,8 +1,8 @@
 import type { IAuthenticationService } from '@/application/services/auth.service.interface';
 import { ILogEventUseCase } from '@/application/use-cases/log-event.use-case';
-import { UnauthenticatedError } from '@/entities/errors/auth';
 import { InputParseError } from '@/entities/errors/common';
 import { z } from 'zod';
+
 const LogEventDTO = z.object({
     sessionId: z.number().int(),
     actionName: z.string().min(1).max(100),
@@ -11,6 +11,7 @@ const LogEventDTO = z.object({
     stepId: z.number().int().optional(),
     payload: z.string().optional(),
 });
+
 export type ICreateEventController = ReturnType<typeof createEventController>;
 export const createEventController =
     (
@@ -25,15 +26,22 @@ export const createEventController =
                 cause: parsed.error,
             });
         }
+
         const data = parsed.data;
         const userId = await authenticationService.getCurrentUserId();
-        if (userId === null)
-            throw new UnauthenticatedError('User must be logged in.');
+        if (userId === null) {
+            // This might happen if the user navigates to a page they are not authorized to visit
+            // They will be redirected, but we do not need to throw an error here
+            console.error('Unauthenticated user tried to log event', { data });
+            return null;
+        }
+
         // Ensure that payload is string
         const payloadString =
             typeof data.payload === 'string'
                 ? data.payload
                 : JSON.stringify(data.payload);
+
         const out = await logEventUseCase.execute({
             userId,
             sessionId: data.sessionId,
@@ -44,5 +52,6 @@ export const createEventController =
             stepId: data.stepId,
             payload: payloadString,
         });
+
         return { id: out.id, loggedAt: out.loggedAt };
     };
