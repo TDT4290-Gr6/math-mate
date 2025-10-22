57051e299f977db5779a8fcac4a3b8cc7a8d19af
Runar Saur Modahl <runarmod@gmail.com>
2025-10-22 19:41:59 +0200

    feat(e2e): test country selection (#148)

---
+++ cypress/e2e/country-select.cy.ts
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+describe('country select', () => {
+    it('displays country options', () => {
+        cy.login('cypress-country-select');
+        cy.visit('/');
+        cy.contains('Country of residence:').should('be.visible');
+        cy.get('button')
+            .contains('Select country')
+            .should('be.visible')
+            .click();
+        cy.get('[data-slot="command-item"]')
+            .contains('Norway')
+            .should('be.visible');
+    });
+
+    it('allows selecting a country', () => {
+        cy.login('cypress-country-select');
+        cy.visit('/');
+        cy.contains('Country of residence:').should('be.visible');
+        cy.get('button')
+            .contains('Select country')
+            .should('be.visible')
+            .click();
+        cy.get('[data-slot="command-item"]')
+            .contains('Norway')
+            .should('be.visible')
+            .click();
+        cy.get('button').contains('Submit').should('be.visible').click();
+        cy.contains('Country of residence:').should('not.exist');
+    });
+});
