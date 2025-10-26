096c99108db4348fd97ef73fabba1881093c8e86
Axelhovde <160112522+Axelhovde@users.noreply.github.com>
2025-10-26 07:51:20 +0000

    fix(format): autofix code style

--- app/protected/problem/page.tsx
+++ app/protected/problem/page.tsx
@@ -4,10 +4,10 @@ import { useTrackedLogger } from '@/components/logger/LoggerProvider';
 import SubjectSelectPopup from '@/components/subject-select-popup';
 import type { Problem } from '@/entities/models/problem';
 import ProblemCard from '@/components/ui/problem-card';
-import { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import Header from '@/components/ui/header';
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import { getProblems } from 'app/actions';
 
 /**
