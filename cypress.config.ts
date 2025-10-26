0f27b13bf9653ef01de94c5425209e8443ca6528
Runar Saur Modahl <runarmod@gmail.com>
2025-10-26 20:17:05 +0100

    fix(e2e): delete users from db after ALL e2e tests are done (#183)

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,14 +5,11 @@ export default defineConfig({
     e2e: {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         setupNodeEvents(on, config) {
-            on('task', {
-                async deleteCypressUsers() {
-                    console.log('Deleting Cypress test users...');
-                    await prisma.user.deleteMany({
-                        where: { uuid: { startsWith: 'cypress' } },
-                    });
-                    return null;
-                },
+            on('after:run', async () => {
+                console.log('Deleting Cypress test users...');
+                await prisma.user.deleteMany({
+                    where: { uuid: { startsWith: 'cypress' } },
+                });
             });
         },
         baseUrl: 'http://localhost:3000',
--- cypress/support/commands.d.ts
+++ cypress/support/commands.d.ts
@@ -1,7 +1,21 @@
 declare global {
     namespace Cypress {
         interface Chainable {
-            login(uuid: string): Chainable;
+            /**
+             * Custom command to log in a user with a given UUID. The session is cached per UUID for
+             * a cypress test run.
+             *
+             * If no UUID is provided, a unique one is generated to create a new user. This is
+             * useful if you require a completely fresh user for each test (e.g. you require the
+             * country select popup, or no previously solved problems).
+             *
+             * If a UUID is provided, the same user will be used across tests (useful for speeding
+             * up tests, as it avoids re-creating the user).
+             *
+             * @param uuid - The UUID of the user to log in. Must start with 'cypress' to identify
+             * test users.
+             */
+            login(uuid?: string): Chainable;
         }
     }
 }
