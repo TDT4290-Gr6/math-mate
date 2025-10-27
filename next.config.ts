4b7f447d446f154437a1d6ece9ddf0304a41ed92
Runar Saur Modahl <runarmod@gmail.com>
2025-10-27 21:43:17 +0100

    fix: update comparison operator and ignore more on docker build (#190)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    output: process.env.STANDALONE_BUILD == 'true' ? 'standalone' : undefined,
+    output: process.env.STANDALONE_BUILD === 'true' ? 'standalone' : undefined,
 };
 
 export default nextConfig;
